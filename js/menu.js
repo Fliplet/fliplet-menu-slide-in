2de4ebebb5e5e489f5ac210c93324c8f96ef0f8f
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-09-29 12:22:59 +0300

    [FIX]

--- js/menu.js
+++ js/menu.js
@@ -18,7 +18,7 @@ function init() {
       '<i class="fa fa-fw fa-sign-out"></i>',
       '</div>',
       '<i class="fa fa-angle-right linked-icon" aria-hidden="true"></i>',
-      '<span class="internal-link buttonControl">' + T('widgets.menu.slideInBar.actions.exit') + '</span>',
+      '<span class="internal-link buttonControl">' + T('widgets.menu.slideIn.actions.exit') + '</span>',
       '</li>'
     ].join(''));
 
